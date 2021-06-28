bc5e154974521ae9cf24954dc9567205edea914c
wu529778790 <529778790@qq.com>
2021-06-28 22:06:58 +0800

    初始化删除无用内容

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import dataV from "@jiaminghi/data-view";
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.use(dataV);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
