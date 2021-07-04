ee7129f29f605a8896bf0f7636830b025a895fca
wu529778790 <529778790@qq.com>
2021-07-05 00:52:18 +0800

    add配置

--- src/views/config/setting.vue
+++ src/views/config/setting.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="drawer-container">
-        <div>
+        <div class="drawer-box">
             <h3 class="drawer-title">设置(立即生效)</h3>
             <div class="drawer-item">
                 <span>标题:</span>
@@ -65,7 +65,6 @@
                     <span>title:</span>
                     <el-input
                         v-model="chartstitle"
-                        placeholder="请输入颜色"
                         type="textarea"
                         autosize
                         @change="value => changeSetting('chartstitle', value)"
@@ -73,9 +72,62 @@
                 </div>
                 <span class="description">title用来显示图表的标题，你可以配置该项来设置其位置和样式</span>
             </div>
-
-            <el-button type="primary" @click.native="reload" size="mini">刷新生效</el-button>
-            <el-button type="primary" @click.native="reload" size="mini">上传后台</el-button>
+            <div class="charts-item">
+                <div class="drawer-item">
+                    <span>legend:</span>
+                    <el-input
+                        v-model="chartslegend"
+                        type="textarea"
+                        autosize
+                        @change="value => changeSetting('chartslegend', value)"
+                    ></el-input>
+                </div>
+                <span class="description">legend为图表提供图例标签功能，它允许用户隐藏/显示series中的某个子图表</span>
+            </div>
+            <div class="charts-item">
+                <div class="drawer-item">
+                    <span>xAxis:</span>
+                    <el-input
+                        v-model="chartsxAxis"
+                        placeholder="请输入颜色"
+                        type="textarea"
+                        autosize
+                        @change="value => changeSetting('chartsxAxis', value)"
+                    ></el-input>
+                </div>
+                <span class="description">xAxis用来配置x坐标轴，它的值可以为对象，也可以为一个数组，为数组时即多个x坐标轴(至多2个)</span>
+            </div>
+            <div class="charts-item">
+                <div class="drawer-item">
+                    <span>yAxis:</span>
+                    <el-input
+                        v-model="chartsyAxis"
+                        placeholder="请输入颜色"
+                        type="textarea"
+                        autosize
+                        @change="value => changeSetting('chartsyAxis', value)"
+                    ></el-input>
+                </div>
+                <span class="description">yAxis用来配置y坐标轴，它的值可以为对象，也可以为一个数组，为数组时即多个y坐标轴(至多2个)</span>
+            </div>
+            <div class="charts-item">
+                <div class="drawer-item">
+                    <span>chartsgrid:</span>
+                    <el-input
+                        v-model="chartsxAxis"
+                        placeholder="请输入颜色"
+                        type="textarea"
+                        autosize
+                        @change="value => changeSetting('chartsxAxis', value)"
+                    ></el-input>
+                </div>
+                <span class="description">grid是配合坐标系工作的，通过配置它，你可以设置坐标系的宽高大小</span>
+            </div>
+            <div class="footer">
+                <el-button type="primary" @click.native="resize" size="mini">恢复默认</el-button>
+                <el-button type="primary" @click.native="reload" size="mini">刷新生效</el-button>
+                <el-button type="primary" @click.native="reload" size="mini">上传后台</el-button>
+            </div>
         </div>
     </div>
 </template>
@@ -121,6 +173,10 @@ export default {
         this.globalBorder = this.setting.globalBorder
         this.chartscolor = Array.isArray(this.setting.chartscolor) ? this.setting.chartscolor : [this.setting.chartscolor]
         this.chartstitle = JSON.stringify(this.setting.chartstitle, null, 4)
+        this.chartslegend = JSON.stringify(this.setting.chartslegend, null, 4)
+        this.chartsxAxis = JSON.stringify(this.setting.chartsxAxis, null, 4)
+        this.chartsyAxis = JSON.stringify(this.setting.chartsyAxis, null, 4)
+        this.chartsgrid = JSON.stringify(this.setting.chartsgrid, null, 4)
     },
     methods: {
         changeSetting(key, value) {
@@ -132,6 +188,9 @@ export default {
             console.log(key, value)
             this.$store.dispatch('setting/changeSetting', { key, value })
         },
+        resize() {
+            localStorage.clear()
+        },
         reload() {
             location.reload()
         }
@@ -141,13 +200,12 @@ export default {
   
   <style lang="scss" scoped>
 .drawer-container {
-    padding: 24px;
+    padding: 5px 20px;
     font-size: 14px;
     line-height: 1.5;
     word-wrap: break-word;
-    overflow: hidden;
-    height: 100%;
-
+    height: 98vh;
+    overflow: auto;
     .drawer-title {
         margin-bottom: 12px;
         color: rgba(0, 0, 0, 0.85);
@@ -173,6 +231,13 @@ export default {
         .description {
             color: rgba(0, 0, 0, 0.65);
         }
+    }
+    .footer {
+        position: sticky;
+        left: 0;
+        bottom: 0;
+        display: flex;
+        justify-content: space-between;
     }
 }
 </style>
