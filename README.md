4a3e73b09eb7b773416a89437f5ba3cb2a2cd64f
wu529778790 <529778790@qq.com>
2021-07-11 22:07:21 +0800

    更新update

--- README.md
+++ README.md
@@ -2,14 +2,20 @@
 
 利用 vue,charts 封装的可视化大屏快速搭建模板
 
+预览链接：<https://blog.shenzjd.com/vue-screen/#/home>
+
 URL 后面加参数?config=1 开启配置模式
+
+配置模式预览链接：<https://blog.shenzjd.com/vue-screen/#/home?config=1>
 
 - 标题
 - 背景颜色
 - 字体颜色
 - 边框
 
-  ![20210705010323](https://cdn.jsdelivr.net/gh/wu529778790/image/blog/20210705010323.png)
+![20210711220355](https://cdn.jsdelivr.net/gh/wu529778790/image/blog/20210711220355.png)
+
+配置模式会自动加上红色边框，可以在右下角拖拽，点击组件可以拖动
 
 ## 注意
 
@@ -18,6 +24,8 @@ URL 后面加参数?config=1 开启配置模式
 在 vue-grid-layout 渲染完以后在开始渲染里面的内容,保证 echarts 在渲染之前就可以拿到真实的宽高
 
 所以在 layoutReadyEvent 事件中做了控制
+
+每次拖拽或者移动修改 layout 的数据的时候，都会从新渲染页面(利用这种模式强制重新渲染拿到实际的宽高，有人有更好的方法可以给我提 PR)
 
 ## 参考链接
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -193,7 +193,42 @@ export default {
           },
         ],
       },
-      option4: {},
+      option4: {
+        legend: {
+          top: "bottom",
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            mark: { show: true },
+            dataView: { show: true, readOnly: false },
+            restore: { show: true },
+            saveAsImage: { show: true },
+          },
+        },
+        series: [
+          {
+            name: "面积模式",
+            type: "pie",
+            radius: [50, 250],
+            center: ["50%", "50%"],
+            roseType: "area",
+            itemStyle: {
+              borderRadius: 8,
+            },
+            data: [
+              { value: 40, name: "rose 1" },
+              { value: 38, name: "rose 2" },
+              { value: 32, name: "rose 3" },
+              { value: 30, name: "rose 4" },
+              { value: 28, name: "rose 5" },
+              { value: 26, name: "rose 6" },
+              { value: 22, name: "rose 7" },
+              { value: 18, name: "rose 8" },
+            ],
+          },
+        ],
+      },
       option5: {
         title: {
           text: "未来一周气温变化",
@@ -274,7 +309,50 @@ export default {
           },
         ],
       },
-      option6: {},
+      option6: {
+        angleAxis: {
+          type: "category",
+          data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+        },
+        radiusAxis: {},
+        polar: {},
+        series: [
+          {
+            type: "bar",
+            data: [1, 2, 3, 4, 3, 5, 1],
+            coordinateSystem: "polar",
+            name: "A",
+            stack: "a",
+            emphasis: {
+              focus: "series",
+            },
+          },
+          {
+            type: "bar",
+            data: [2, 4, 6, 1, 3, 2, 1],
+            coordinateSystem: "polar",
+            name: "B",
+            stack: "a",
+            emphasis: {
+              focus: "series",
+            },
+          },
+          {
+            type: "bar",
+            data: [1, 2, 3, 4, 1, 2, 5],
+            coordinateSystem: "polar",
+            name: "C",
+            stack: "a",
+            emphasis: {
+              focus: "series",
+            },
+          },
+        ],
+        legend: {
+          show: true,
+          data: ["A", "B", "C"],
+        },
+      },
       option7: {
         title: {
           text: "世界人口总量",
