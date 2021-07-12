93438a312d34623fb4daedde126f8c7870659b89
01388213 <weiwu3@sfmail.sf-express.com>
2021-07-12 16:28:56 +0800

    修改配置边框颜色

--- src/components/RightPanel/index.vue
+++ src/components/RightPanel/index.vue
@@ -1,152 +1,150 @@
 <template>
-    <div ref="rightPanel" :class="{ show: show }" class="rightPanel-container">
-        <div class="rightPanel-background" />
-        <div class="rightPanel">
-            <div
-                class="handle-button"
-                :style="{ 'top': buttonTop + 'px', 'background-color': setting.bgColor }"
-                @click="show = !show"
-            >
-                <i :class="show ? 'el-icon-close' : 'el-icon-setting'" />
-            </div>
-            <div class="rightPanel-items">
-                <slot />
-            </div>
-        </div>
+  <div ref="rightPanel" :class="{ show: show }" class="rightPanel-container">
+    <div class="rightPanel-background" />
+    <div class="rightPanel">
+      <div
+        class="handle-button"
+        :style="{ top: buttonTop + 'px', 'background-color': setting.bgColor }"
+        @click="show = !show"
+      >
+        <i :class="show ? 'el-icon-close' : 'el-icon-setting'" />
+      </div>
+      <div class="rightPanel-items">
+        <slot />
+      </div>
     </div>
+  </div>
 </template>
-  
-  <script>
-import { addClass, removeClass } from '@/utils'
-import { mapGetters } from 'vuex'
+
+<script>
+import { addClass, removeClass } from "@/utils";
+import { mapGetters } from "vuex";
 
 export default {
-    name: 'RightPanel',
-    props: {
-        clickNotClose: {
-            default: false,
-            type: Boolean
-        },
-        buttonTop: {
-            default: 250,
-            type: Number
-        }
+  name: "RightPanel",
+  props: {
+    clickNotClose: {
+      default: false,
+      type: Boolean,
     },
-    data() {
-        return {
-            show: false
-        }
+    buttonTop: {
+      default: 250,
+      type: Number,
     },
-    computed: {
-        ...mapGetters([
-            'setting'
-        ])
+  },
+  data() {
+    return {
+      show: false,
+    };
+  },
+  computed: {
+    ...mapGetters(["setting"]),
+  },
+  watch: {
+    show(value) {
+      if (value && !this.clickNotClose) {
+        this.addEventClick();
+      }
+      if (value) {
+        addClass(document.body, "showRightPanel");
+      } else {
+        removeClass(document.body, "showRightPanel");
+      }
     },
-    watch: {
-        show(value) {
-            if (value && !this.clickNotClose) {
-                this.addEventClick()
-            }
-            if (value) {
-                addClass(document.body, 'showRightPanel')
-            } else {
-                removeClass(document.body, 'showRightPanel')
-            }
-        }
+  },
+  mounted() {
+    this.insertToBody();
+  },
+  beforeDestroy() {
+    const elx = this.$refs.rightPanel;
+    elx.remove();
+  },
+  methods: {
+    addEventClick() {
+      window.addEventListener("click", this.closeSidebar);
     },
-    mounted() {
-        this.insertToBody()
+    closeSidebar(evt) {
+      const parent = evt.target.closest(".rightPanel");
+      if (!parent) {
+        this.show = false;
+        window.removeEventListener("click", this.closeSidebar);
+      }
     },
-    beforeDestroy() {
-        const elx = this.$refs.rightPanel
-        elx.remove()
+    insertToBody() {
+      const elx = this.$refs.rightPanel;
+      const body = document.querySelector("body");
+      body.insertBefore(elx, body.firstChild);
     },
-    methods: {
-        addEventClick() {
-            window.addEventListener('click', this.closeSidebar)
-        },
-        closeSidebar(evt) {
-            const parent = evt.target.closest('.rightPanel')
-            if (!parent) {
-                this.show = false
-                window.removeEventListener('click', this.closeSidebar)
-            }
-        },
-        insertToBody() {
-            const elx = this.$refs.rightPanel
-            const body = document.querySelector('body')
-            body.insertBefore(elx, body.firstChild)
-        }
-    }
-}
+  },
+};
 </script>
-  
-  <style>
+
+<style>
 .showRightPanel {
-    overflow: hidden;
-    position: relative;
-    width: calc(100% - 15px);
+  overflow: hidden;
+  position: relative;
+  width: calc(100% - 15px);
 }
 </style>
-  
+
 <style lang="scss" scoped>
 .rightPanel-background {
-    position: fixed;
-    top: 0;
-    left: 0;
-    opacity: 0;
-    transition: opacity 0.3s cubic-bezier(0.7, 0.3, 0.1, 1);
-    background: rgba(0, 0, 0, 0.2);
-    z-index: -1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  transition: opacity 0.3s cubic-bezier(0.7, 0.3, 0.1, 1);
+  background: rgba(0, 0, 0, 0.2);
+  z-index: -1;
 }
 
 .rightPanel {
-    width: 100%;
-    max-width: 500px;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    right: 0;
-    box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.05);
-    transition: all 0.25s cubic-bezier(0.7, 0.3, 0.1, 1);
-    transform: translate(100%);
-    background: #fff;
-    z-index: 2000;
+  width: 100%;
+  max-width: 500px;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  right: 0;
+  box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.05);
+  transition: all 0.25s cubic-bezier(0.7, 0.3, 0.1, 1);
+  transform: translate(100%);
+  background: #fff;
+  z-index: 2000;
 }
 
 .show {
-    transition: all 0.3s cubic-bezier(0.7, 0.3, 0.1, 1);
+  transition: all 0.3s cubic-bezier(0.7, 0.3, 0.1, 1);
 
-    .rightPanel-background {
-        z-index: 1000;
-        opacity: 1;
-        width: 100%;
-        height: 100%;
-    }
+  .rightPanel-background {
+    z-index: 1000;
+    opacity: 1;
+    width: 100%;
+    height: 100%;
+  }
 
-    .rightPanel {
-        transform: translate(0);
-    }
+  .rightPanel {
+    transform: translate(0);
+  }
 }
 
 .handle-button {
-    width: 48px;
-    height: 48px;
-    position: absolute;
-    left: -48px;
-    text-align: center;
+  width: 48px;
+  height: 48px;
+  position: absolute;
+  left: -50px;
+  text-align: center;
+  font-size: 24px;
+  border: 1px solid red;
+  border-right: none;
+  border-radius: 6px 0 0 6px !important;
+  z-index: 0;
+  pointer-events: auto;
+  cursor: pointer;
+  color: red;
+  line-height: 48px;
+  i {
     font-size: 24px;
-    border: 1px solid white;
-    border-radius: 6px 0 0 6px !important;
-    z-index: 0;
-    pointer-events: auto;
-    cursor: pointer;
-    color: #fff;
     line-height: 48px;
-    i {
-        font-size: 24px;
-        line-height: 48px;
-    }
+  }
 }
 </style>
-  
